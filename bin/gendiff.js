87edbebbf53f7521f42cb7daaabf2b3189dc4a81
hvaheterduminvenn <roma.bond.geo@yandex.ru>
2022-11-27 12:51:26 +0300

    command args added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  // .argument('<filepath1>', 'file #1 path')
+  // .argument('<filepath2>', 'file #2 path')
   .version('0.1.0')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
